5f775319db0c8554456a1df4d118572856006447
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-03-02 16:38:56 +0900

    [refactor] myPageRepository 삭제하고 이벤트 repository로 함수 이동 (#93)

--- src/main/java/hyangyu/server/repository/FavoriteFestivalRepository.java
+++ src/main/java/hyangyu/server/repository/FavoriteFestivalRepository.java
@@ -1,16 +1,31 @@
 package hyangyu.server.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import hyangyu.server.domain.Festival;
 import hyangyu.server.domain.FavoriteFestival;
 import hyangyu.server.domain.FavoriteFestivalId;
+import hyangyu.server.domain.QFavoriteFestival;
+import hyangyu.server.domain.QFestival;
+import hyangyu.server.dto.event.EventDto;
+import hyangyu.server.dto.event.FestivalDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class FavoriteFestivalRepository {
     private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+    QFestival festival = QFestival.festival;
+    QFavoriteFestival favoriteFestival = QFavoriteFestival.favoriteFestival;
+
+    public FavoriteFestivalRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
 
     public void saveFavoriteFestival(FavoriteFestival favoriteFestival) {
         em.persist(favoriteFestival);
@@ -21,4 +36,28 @@ public class FavoriteFestivalRepository {
     }
 
     public void deleteFavoriteFestival(FavoriteFestival favoriteFestival) { em.remove(em.find(FavoriteFestival.class, favoriteFestival.getFavoriteFestivalId()));}
+
+
+    public FestivalDto getFavoriteFestival(Long userId, int page) {
+
+        List<Festival> festivals = queryFactory.select(favoriteFestival.festival)
+                .from(favoriteFestival)
+                .where(favoriteFestival.user.userId.castToNum(Long.class).eq(userId))
+                .orderBy(favoriteFestival.festival.endDate.asc())
+                .offset(page*10)
+                .limit(10)
+                .fetch();
+
+        List<EventDto> festivalList = new ArrayList<>();
+        for (Festival festival : festivals) {
+            EventDto eventDto = new EventDto(festival);
+            eventDto.setSaved(true);
+            festivalList.add(eventDto);
+        }
+
+        FestivalDto myFestival = new FestivalDto(festivalList);
+
+        return myFestival;
+
+    }
 }
