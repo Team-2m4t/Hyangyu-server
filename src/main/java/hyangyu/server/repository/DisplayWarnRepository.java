d8c808ce624feda3b0aff73d4dbc4842c31b9dfe
yujin113 <claire9585@sookmyung.ac.kr>
2022-05-06 12:43:57 +0900

    [fix] 리뷰신고Repository 메서드 변경 (#104)

--- src/main/java/hyangyu/server/repository/DisplayWarnRepository.java
+++ src/main/java/hyangyu/server/repository/DisplayWarnRepository.java
@@ -6,13 +6,15 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 public interface DisplayWarnRepository extends JpaRepository<DisplayWarn, Long> {
 
     @Query("select w from DisplayWarn w where w.displayReview.reviewId=?1 and w.user.userId=?2")
-    public DisplayWarn getDisplayWarn(Long reviewId, Long userId);
+    Optional<DisplayWarn> getDisplayWarn(Long reviewId, Long userId);
 
     @Modifying
     @Transactional
     @Query("delete from DisplayWarn w where w.displayReview.reviewId=?1")
-    public void deleteDisplayWarns(Long reviewId);
+    void deleteDisplayWarns(Long reviewId);
 }
--- src/main/java/hyangyu/server/repository/FestivalWarnRepository.java
+++ src/main/java/hyangyu/server/repository/FestivalWarnRepository.java
@@ -6,13 +6,15 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 public interface FestivalWarnRepository extends JpaRepository<FestivalWarn, Long> {
 
     @Query("select w from FestivalWarn w where w.festivalReview.reviewId=?1 and w.user.userId=?2")
-    public FestivalWarn getFestivalWarn(Long reviewId, Long userId);
+    Optional<FestivalWarn> getFestivalWarn(Long reviewId, Long userId);
 
     @Modifying
     @Transactional
     @Query("delete from FestivalWarn w where w.festivalReview.reviewId=?1")
-    public void deleteFestivalWarns(Long reviewId);
+    void deleteFestivalWarns(Long reviewId);
 }
